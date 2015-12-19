d045e476c54f6a0b79c7216afb4cd9518b3a10b6
Alexander Dean <dstar@slackless.com>
2015-12-19 14:13:28 -0800

    Turn on stack concurrency.

--- src/eval/sink_stacking.erl
+++ src/eval/sink_stacking.erl
@@ -1,21 +1,44 @@
 -module(sink_stacking).
 
 %% API
--export([run/0,run/1]).
+-export([run/0,run/1,run_timed/0]).
 
-run() -> run({mytestevent, "Hello World"}).
-run(MockEvent) ->
-  StackSink = create_stack([
-    {libemp_stack_sink, [{stack,[
+-define(EVENT, {mytestevent, "Hello World"}).
+-define(STACK(Sinks),[{stack,Sinks}]).
+-define(SUB_STACK(Sinks), {libemp_stack_sink, ?STACK(Sinks)}).
+
+%% Create a stack like:
+%%    [ [hang], [ log, context, log ], log ]
+stack() ->
+  {ok, Stack} = libemp_sink:setup(libemp_stack_sink, ?STACK([
+    ?SUB_STACK([
+      {libemp_hang_sink, [5000]} % Hang for 5 seconds.
+    ]),
+    ?SUB_STACK([
       {libemp_logger_sink, []},  % Log the new event,
       {libemp_context_sink, []}, % Add context to it,
-      {libemp_logger_sink, []}   % the log it again to show it works.
-    ]}]},
+      {libemp_logger_sink, []}   % the log it again to show it has been updated.
+    ]),
     {libemp_logger_sink,[]} % Show that any context within the substack is lost.
-  ]),
-  MockBuffer = [],
-  libemp_sink:process(MockEvent, MockBuffer, StackSink).
-
-create_stack(Sinks) ->
-  {ok, Stack} = libemp_sink:setup(libemp_stack_sink, [{stack,Sinks}]),
+  ])),
   Stack.
+
+%%  which can run in sequence or in parallel:
+%%    the substacks can be thrown into other threads to finish parsing while
+%%    the parent stack continues with the rest of its processing (i.e. the last
+%%    sink). Only when it hits a sink does it HAVE to run in sequence.
+run() -> run(?EVENT).
+run(MockEvent) -> libemp_sink:process(MockEvent, fakebuffer, stack()).
+
+run_timed() ->
+  io:format("Now building a stack of two substacks, 5 sinks in total.~n"
+            "i.e. [ [hang], [ log, context, log ], log ]~n"),
+   {Time, Stack} = timer:tc(fun stack/0),
+  io:format("It took ~p μs.~n", [Time]),
+
+  io:format("Then we'll go ahead and process an event and have one of the~n"
+            "sinks hang for 5 seconds to simulate a long running process.~n"),
+  {Time2, _Res} = timer:tc(libemp_sink, process, [?EVENT, fakebuffer, Stack]),
+  io:format("It took ~p μs.~n", [Time2]),
+  io:format("Thus overhead is: ~p μs.~n", [Time2-5000000]).
+
--- src/sinks/stack/libemp_stack_future.erl
+++ src/sinks/stack/libemp_stack_future.erl
@@ -10,32 +10,47 @@
 %% API
 -export([new/2,join/1]).
 
-%TODO: Fix these fake futures, they should only be turned on if sequential/debug.
-%-ifdef(DEBUG).
+-ifdef(DEBUG).
 new( Fun, Args ) when is_list(Args) andalso is_function(Fun, length(Args)) ->
-  erlang:apply(Fun, Args).
+  catch erlang:apply(Fun, Args).
 
 join(Result) -> Result.
-%-else.
-% TODO: These should actually use constrained parallelism via epocxy.
-%new( Fun, Args ) when is_list(Args) andalso is_function(Fun,length(Args)) ->
-%   Pid = spawn( wrap_to_hang_for_join( Fun, Args ) ),
-%   {Pid}.
-%
-%join( {Pid}=_Future ) ->
-%   notify_for_join( Pid ),
-%   hang_for_join().
-%
-%-endif.
+
+-else.
+%% FIXME?: These could actually use constrained parallelism via epocxy;
+%%  However for now it should be somewhat constrained due to the fact that
+%%  all events are processed in sequence via processors. Its fairly
+%%  deterministic how many potential threads will be open: i.e.
+%%           ( # of Stacks ) X ( # of processors running them )
+
+new( Fun, Args ) when is_list(Args) andalso is_function(Fun,length(Args)) ->
+   spawn( wrap_to_hang_for_join( Fun, Args ) ).
+
+join( Future ) ->
+   notify_for_join( Future ),
+   hang_for_join().
+-endif.
 
 %%% ==========================================================================
 %%% Internal Functionality
 %%% ==========================================================================
 
-%% FIXME?: the following is gross for noisy joiners.
+%% @hidden
+%% @doc Wrap function application with closure and messaging system to send
+%%   results back to joiner.
+%% @end
 wrap_to_hang_for_join( Fun, Args ) ->
   fun() -> future_hanger( catch erlang:apply( Fun, Args ) ) end.
+
+%% @hidden
+%% @doc Hang for a message from the joiner so that we can send it our result.
 future_hanger( Result ) -> receive {joiner,Pid} -> Pid!{future,Result} end.
-notify_for_join( Pid ) -> Pid!{joiner,self()}.
+
+%% @hidden
+%% @doc Send the Future the address to the local mailbox for results.
+notify_for_join( Future ) -> Future ! {joiner,self()}.
+
+%% @hidden
+%% @doc Wait for the future to complete so that we get the results back.
 hang_for_join() -> receive {future,Result} -> Result end.
 
